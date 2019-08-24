b5269417ae1e4299cce55b2b49edcac6fd480d97
1895212 <1895212@E21032.CEGEPGIM.LCL>
2019-08-24 13:38:01 -0400

    Added Libraries

--- SalesActivity/src/Application/Main.java
+++ SalesActivity/src/Application/Main.java
@@ -1,21 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package Application;
 
-/**
- *
- * @author 1895212
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import DAO.*;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+
+         public static void main(String[] args) throws SQLException {
+       
+         //initialize database connection
+        Connection con = null;
+        PreparedStatement stm = null;
+        
+        
+        try {
+            
+            DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+            con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "sales", "anypw");
+            
+            Client client = new Client(con,stm);
+                                       
+        } 
+        
+        catch (SQLException ex){
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+        finally    {
+
+                 if(stm!=null)
+                        {
+                            stm.close();
+                        }
+
+                        if(con!=null)
+                        {
+                               con.close();
+                        }
+                }
+        
+    
+    
+    }
+
 }
